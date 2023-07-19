944efa2692f8571a812b31f98ba4af1aa007600d
JamesB-Blur <berkheimer@blur.com>
2023-07-19 11:39:45 -0700

    Adding isort to the optional dev package
    We want to give devs the option of using isort.
    
    Also added isort: split to the hab/__init__.py

--- hab/__init__.py
+++ hab/__init__.py
@@ -9,19 +9,11 @@ __all__ = [
     'Solver',
 ]
 
-
-import logging
-
-# isort: on # Note: Future imports depend on NotSet so it must be imported here
-# from . import utils
 from .utils import NotSet
 
-# isort: off
-
+# Note: Future imports depend on NotSet so it must be imported first
+# isort: split
 
 from .resolver import Resolver
 from .site import Site
-
 from .version import version as __version__
-
-logger = logging.getLogger(__name__)
