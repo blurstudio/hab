0a3376d7ba2e57a3b17a72cedf5391e265297128
Mike Hendricks <mikeh@blur.com>
2024-12-06 12:19:41 -0800

    3/4: Add hab install system and switch to using DistroFinder
    - Add `hab install` command that resolves all distros required to resolve
    multiple URI's and then download and install any missing distros.
    - Distro resolving is now handled by `DistroFinder` or sub-class.
    - Site now has a downloads dictionary that configures how `hab install`
    can resolve remote distros for installing locally.
    - `Resolver.distro_mode_override` with context can be used to change
    `Resolver.distro`'s output to the `Site.downloads["distros"] used to find
    remote distros to install.

--- hab/__init__.py
+++ hab/__init__.py
@@ -1,10 +1,10 @@
-__all__ = ["__version__", "NotSet", "Resolver", "Site"]
+__all__ = ["__version__", "DistroMode", "NotSet", "Resolver", "Site"]
 
 from .utils import NotSet
 
 # Note: Future imports depend on NotSet so it must be imported first
 # isort: split
 
-from .resolver import Resolver
+from .resolver import DistroMode, Resolver
 from .site import Site
 from .version import version as __version__
